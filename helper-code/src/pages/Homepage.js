7ac97a62a9d575558b04eb8800304196ae458564
aurora <aurora.makovac@gmail.com>
2023-03-31 14:36:26 +0200

    playing around with implemented components

--- helper-code/src/pages/Homepage.js
+++ helper-code/src/pages/Homepage.js
@@ -6,21 +6,23 @@ import CryptoIcon from "../components/CryptoIcon/CryptoIcon";
 import CurrencySwitch from "../components/CurrencySwitch/CurrencySwitch";
 import DateTimeAgo from "../components/DateTime/DateTimeAgo";
 import ExternalLink from "../components/ExternalLink/ExternalLink";
-import Graph from "../components/Graph/Graph";
 import GraphSwitch from "../components/GraphSwitch/GraphSwitch";
 import Info from "../components/Info/Info";
 import Loader from "../components/Loader/Loader";
 import RangeSlider from "../components/RangeSlider/RangeSlider";
 import SearchInput from "../components/SearchInput/SearchInput";
 import Select from "../components/Select/Select";
-import SideTab from "../components/SideTab/SideTab";
+import SideTabContent from "../components/SideTab/SideTabContent";
+import SideTabNav from "../components/SideTab/SideTabNav";
 import SimpleRedirect from "../components/SimpleRedirect/SimpleRedirect";
 import Stat from "../components/Stat/Stat";
-import Stats from "../components/Stats/Stats";
+import StatsBar from "../components/Stats/StatsBar";
 import Switch from "../components/Switch/Switch";
 import TabCard from "../components/TabCard/TabCard";
-import Table from "../components/Table/Table";
-import Tabs from "../components/Tabs/Tabs";
+//import LinkTable from "../components/Table/LinkTable";
+//import RemoteTable from "../components/Table/RemoteTable";
+// TODO: resolve problem with bootstrap table
+import IconTabs from "../components/Tabs/IconTabs";
 import TimeSwitch from "../components/TimeSwitch/TimeSwitch";
 import Title from "../components/Title/Title";
 import Value from "../components/Value/Value";
@@ -37,21 +39,29 @@ function HomePage(props) {
           <li>CurrencySwitch: <CurrencySwitch /></li>
           <li>DateTimeAgo: <DateTimeAgo /></li>
           <li>ExternalLink: <ExternalLink /></li>
-          <li>Graph: <Graph /></li>
           <li>GraphSwitch: <GraphSwitch /></li>
           <li>Info: <Info /></li>
           <li>Loader: <Loader /></li>
-          <li>RangeSlider: <RangeSlider /></li>
-          <li>SearchInput: <SearchInput /></li>
+          <li>RangeSlider: <RangeSlider title="title" /></li>
+          <li>SearchInput: <SearchInput onSearch={() => console.log("log me")} /></li>
           <li>Select: <Select /></li>
-          <li>SideTab: <SideTab /></li>
-          <li>SimpleRedirect: <SimpleRedirect /></li>
+          <li>SideTab: <SideTabContent activeTab="a string" tabs={[{id: "sdf", content: <CurrencySwitch />}]}/></li>
+          <li>SideTab: <SideTabNav activeTab="a string" toggleTab={() => console.log("log me")} tabs={[{id: "sdf", text: "some text"}]}/></li>
+          {/* <li>SideTabNav: <SimpleRedirect /></li> --> */}
           <li>Stat: <Stat /></li>
-          <li>Stats: <Stats /></li>
-          <li>Switch: <Switch /></li>
-          <li>TabCard: <TabCard /></li>
-          <li>Table: <Table /></li>
-          <li>Tabs: <Tabs /></li>
+          <li>Stats: <StatsBar stats={[{title: "title", bigValue:"12", normalValue:"23", smallValue:"1" }]} /></li>
+          <li>Switch:           
+          <Switch
+            label="show only non-insolvent"
+            className="mb-2"
+            checked={true}
+            onCheckedChange={(checked) => console.log(checked)}
+          /></li>
+          <li>TabCard: <TabCard tabs={["tab1", "tab2"]} fullHeight={false} /></li>
+          {/* <li>LinkTable: <LinkTable /></li>
+          <li>RemoteTable: <RemoteTable page={12} pageSize={23} totalPageSize={230} /></li> */}
+          <li>IconTabs: <IconTabs tabs={["tab1", "tab2"]} fullHeight={false} /></li>
+          <li>Title: <Title tag="title">Title</Title> </li>
           <li>TimeSwitch: <TimeSwitch /></li>
           <li>Value: <Value /></li>
         </ul>        
