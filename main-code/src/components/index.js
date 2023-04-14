a8fe12cf9438269f9076bc6cf2bed56c07b7bf7f
aurora <aurora.makovac@gmail.com>
2023-04-14 14:22:58 +0200

    background add

--- main-code/src/components/index.js
+++ main-code/src/components/index.js
@@ -1,13 +1,11 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Layout from './Layout';
-import BackgroundCircle from './backgroundCircle';
 import AccordionTile from './accordion/AccordionTile';
 
 export {
   Navbar,
   Footer,
   Layout,
-  BackgroundCircle,
   AccordionTile
 };
--- main-code/tailwind.config.js
+++ main-code/tailwind.config.js
@@ -10,9 +10,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--gradient-color-stops))',
-      },
       lineHeight: {
         '20': '5rem',
       },
