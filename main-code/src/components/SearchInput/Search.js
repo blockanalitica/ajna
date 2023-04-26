240523c1e4842277f946295c2d140699a2c04109
Aurora Makovac <aurora.makovac@gmail.com>
2023-04-26 14:15:51 +0200

    adding overlay when looking on search

--- main-code/src/components/SearchInput/Search.js
+++ main-code/src/components/SearchInput/Search.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import Head from "next/head";
 
 const listOfItems = ["ETH", "DAI", "ETH2", "BTC"];
 
 export default function SearchBar() {
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredItems, setFilteredItems] = useState(listOfItems);
+  const [isFocused, setIsFocused] = useState(false);
 
   const handleSearchChange = (event) => {
     const newSearchTerm = event.target.value;
@@ -18,19 +20,33 @@ export default function SearchBar() {
 
   return (
     <div className="relative">
+      <Head>
+        {isFocused && (
+          <style>
+            {`body { overflow: hidden; }
+            .overlay { position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.7); z-index: 20; }
+            .search-bar, .dropdown { position: relative; z-index: 30; }`}
+          </style>
+        )}
+      </Head>
+      {isFocused && (
+        <div className="overlay" onClick={() => setIsFocused(false)} />
+      )}
       <input
         type="text"
         placeholder="Search"
-        className="w-full px-4 py-2 rounded-full bg-gray-100 text-gray-800"
+        className="bg-gray-24 px-4 font-rubik rounded-full border border-gray-20 text-gray-3 text-l  focus:border-gray-15 block w-full pl-7 p-2.5 search-bar"
         value={searchTerm}
         onChange={handleSearchChange}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
       />
-      {filteredItems.length > 0 && (
-        <ul className="absolute z-10 w-full mt-1 py-1 bg-white rounded-md shadow-lg">
+      {isFocused && filteredItems.length > 0 && (
+        <ul className="absolute w-full mt-1 py-1 border border-ajna-plum bg-black rounded-2xl shadow-lg dropdown">
           {filteredItems.map((item) => (
             <li
               key={item}
-              className="px-4 py-2 text-gray-10 hover:bg-gray-200 cursor-pointer"
+              className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer"
               onClick={() => {
                 setSearchTerm(item);
                 setFilteredItems(listOfItems);
