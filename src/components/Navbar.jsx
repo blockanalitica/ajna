c94175562dc461de1a5c5912b17c051aaf3b4c12
Tomaž Šifrer <3967564+tsifrer@users.noreply.github.com>
2023-05-12 17:24:00 +1200

    refactor: navbar, layout and removed obsolete dependencies (#30)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,103 +1,85 @@
+import { useState } from "react";
 import classnames from "classnames";
-import { Disclosure } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import Image from "next/image";
 import DropDownButton from "./button/DropDownButton";
 
 const Navbar = ({ navigation }) => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <>
-      <div className="min-h-full mt-10">
-        <Disclosure as="nav" className="">
-          {({ open }) => (
-            <>
-              <div className="mx-auto max-w-9xl px-4 sm:px-6 lg:px-8">
-                <div className="flex mt-8 items-center justify-between">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <Link href="/" alt="ajna" className="font-bold">
-                        <Image
-                          src="/assets/Images/Logos/AJNA-Logo-LG.svg"
-                          width={130}
-                          height={50}
-                          alt="Ajna Logo"
-                        />
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="flex items-center">
-                    <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4">
-                        {navigation.map((item) => (
-                          <Link
-                            key={item.name}
-                            href={item.href}
-                            className={classnames(
-                              "rounded-md px-4 py-2 text-l font-medium",
-                              {
-                                "text-ajna-aqua": item.current,
-                                "text-gray-300 hover:bg-gray-700 hover:text-white":
-                                  !item.current,
-                              }
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                          >
-                            {item.name}
-                          </Link>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="hidden md:block">
-                    <div className="ml-4 flex items-center md:ml-6">
-                      <DropDownButton />
-                    </div>
-                  </div>
-                  <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
-                    <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring- focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="sr-only">Open main menu</span>
-                      {open ? (
-                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                      ) : (
-                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                      )}
-                    </Disclosure.Button>
-                  </div>
-                </div>
-              </div>
-
-              <Disclosure.Panel className="md:hidden">
-                <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-                  {navigation.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className={classnames(
-                        "block rounded-md px-3 py-2 text-base font-medium",
-                        {
-                          "bg-gray-900 text-white": item.current,
-                          "text-gray-300 hover:bg-gray-700 hover:text-white":
-                            !item.current,
-                        }
-                      )}
-                      aria-current={item.current ? "page" : undefined}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                </div>
-              </Disclosure.Panel>
-
-              <div className="flex justify-end space-y-1 px-2 md:hidden">
-                <DropDownButton />
-              </div>
-            </>
-          )}
-        </Disclosure>
+    <nav className="mt-4">
+      <div className="flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/assets/Images/Logos/AJNA-Logo-LG.svg"
+            width={130}
+            height={50}
+            alt="Ajna"
+          />
+        </Link>
+        <div className="hidden md:flex space-x-4">
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              href={item.href}
+              className={classnames("rounded-md px-4 py-2 text-l font-medium", {
+                "text-ajna-aqua": item.current,
+                "text-gray-300 hover:bg-gray-700 hover:text-white": !item.current,
+              })}
+              aria-current={item.current ? "page" : undefined}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </div>
+        <div className="hidden md:block">
+          <DropDownButton />
+        </div>
+        <div className="-mr-2 flex md:hidden">
+          {/* Mobile menu button */}
+          <button
+            className="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring- focus:ring-offset-2 focus:ring-offset-gray-800"
+            onClick={() => setOpen(!isOpen)}
+          >
+            {isOpen ? (
+              <FontAwesomeIcon icon={faXmark} className="block h-6 w-6" />
+            ) : (
+              <FontAwesomeIcon icon={faBars} className="block h-6 w-6" />
+            )}
+          </button>
+        </div>
       </div>
-    </>
+
+      {isOpen ? (
+        <div>
+          <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+            {navigation.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className={classnames(
+                  "block rounded-md px-3 py-2 text-base font-medium",
+                  {
+                    "bg-gray-900 text-white": item.current,
+                    "text-gray-300 hover:bg-gray-700 hover:text-white": !item.current,
+                  }
+                )}
+                aria-current={item.current ? "page" : undefined}
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      ) : null}
+
+      <div className="md:hidden flex justify-end -mr-3">
+        <DropDownButton />
+      </div>
+    </nav>
   );
 };
 
