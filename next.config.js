dd13ccadb9acf65a2a7ed7eefeaa1ffd36042a73
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2023-07-04 10:07:32 +0200

    fix: network redirects

--- next.config.js
+++ next.config.js
@@ -11,47 +11,9 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: "/ethereum",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/ethereum/:path*",
-        destination: "/:path*",
-        permanent: true,
-      },
-    ];
-  },
-
-  async rewrites() {
-    return [
-      {
         source: "/",
         destination: "/ethereum",
-      },
-      {
-        source: "/pools",
-        destination: "/ethereum/pools",
-      },
-      {
-        source: "/pools/:slug*",
-        destination: "/ethereum/pools/:slug*",
-      },
-      {
-        source: "/tokens",
-        destination: "/ethereum/tokens",
-      },
-      {
-        source: "/tokens/:slug*",
-        destination: "/ethereum/tokens/:slug*",
-      },
-      {
-        source: "/auctions",
-        destination: "/ethereum/auctions",
-      },
-      {
-        source: "/auctions/:slug*",
-        destination: "/ethereum/auctions/:slug*",
+        permanent: true,
       },
     ];
   },
