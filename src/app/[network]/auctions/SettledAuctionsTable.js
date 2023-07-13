edc02aafd959106b348ce3caa69edfadad8081df
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2023-07-13 13:58:37 +0200

    feat: added pool to settled auctions table

--- src/app/[network]/auctions/SettledAuctionsTable.js
+++ src/app/[network]/auctions/SettledAuctionsTable.js
@@ -7,8 +7,12 @@ import { DateTime } from "luxon";
 import Table from "@/components/table/Table";
 import Value from "@/components/value/Value";
 import DateTimeAgo from "@/components/dateTime/DateTimeAgo";
+import CryptoIcon from "@/components/icon/CryptoIcon";
+import Link from "next/link";
+import { useParams } from "next/navigation";
 
 const SettledAuctionsTable = ({ daysAgo }) => {
+  const { network } = useParams();
   const pageSize = 10;
   const [page, setPage] = useState(1);
   const [order, setOrder] = useState("-settle_time");
@@ -36,6 +40,26 @@ const SettledAuctionsTable = ({ daysAgo }) => {
 
   const columns = [
     {
+      header: "Pool",
+      cell: ({ row }) => (
+        <Link
+          href={`/${network}/pools/${row.pool_address}`}
+          className="text-purple-6 hover:underline flex"
+        >
+          <span className="relative hidden md:flex">
+            <CryptoIcon name={row.collateral_token_symbol} className="z-10" />
+            <CryptoIcon
+              name={row.debt_token_symbol}
+              className="relative left-[-10px] z-0"
+            />
+          </span>
+          <span className="font-medium md:pl-2">
+            {row.collateral_token_symbol} / {row.debt_token_symbol}
+          </span>
+        </Link>
+      ),
+    },
+    {
       header: "Borrower",
       cell: ({ row }) => <>{shorten(row.borrower)}</>,
       smallCell: ({ row }) => (
@@ -44,6 +68,9 @@ const SettledAuctionsTable = ({ daysAgo }) => {
           className="sm:hidden"
         />
       ),
+      headerAlign: "center",
+      cellAlign: "center",
+      visibleAfter: "sm",
     },
     {
       header: "Collateral",
