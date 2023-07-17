16037faf6be7230d63b873528352a976543f872e
Tomaž Šifrer <3967564+tsifrer@users.noreply.github.com>
2023-07-17 13:46:03 +0200

    feat: added simpleanalytics (#103)

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 import { Settings as DTSettings } from "luxon";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
@@ -32,6 +33,24 @@ const RootLayout = ({ children }) => {
         </div>
         <div id="ajna-portal"></div>
         <Analytics />
+        {process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? (
+          <>
+            <Script
+              async
+              defer
+              data-hostname="ajna.blockanalitica.com"
+              src="https://scripts.simpleanalyticscdn.com/latest.js"
+            />
+            <noscript>
+              {/* eslint-disable @next/next/no-img-element */}
+              <img
+                src="https://queue.simpleanalyticscdn.com/noscript.gif?hostname=ajna.blockanalitica.com"
+                alt=""
+                referrerpolicy="no-referrer-when-downgrade"
+              />
+            </noscript>
+          </>
+        ) : null}
       </body>
     </html>
   );
