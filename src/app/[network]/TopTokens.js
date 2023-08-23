19a9e3e5ff006e9b2bb7cc4daa0ba9b3accc48d5
Tomaž Šifrer <3967564+tsifrer@users.noreply.github.com>
2023-08-23 14:28:20 +0200

    feat: show total # of pools and tokens on frontpage (#135)

--- src/app/[network]/TopTokens.js
+++ src/app/[network]/TopTokens.js
@@ -1,7 +1,12 @@
+"use client";
+
 import { useFetch } from "@/hooks";
 import TokensTable from "@/components/table/specific/TokensTable";
+import Button from "@/components/button/Button";
+import { useParams } from "next/navigation";
 
 const TopTokens = ({ daysAgo, ...rest }) => {
+  const { network } = useParams();
   const {
     data = {},
     error,
@@ -14,16 +19,22 @@ const TopTokens = ({ daysAgo, ...rest }) => {
   if (error) {
     return <p>Failed to load data</p>;
   }
-  let { results } = data;
+  let { results, count = 0 } = data;
 
   return (
-    <TokensTable
-      data={results}
-      allowOrder={false}
-      isLoading={isLoading}
-      placeholderRows={5}
-      {...rest}
-    />
+    <>
+      <div className="flex flex-row justify-between items-center mb-5">
+        <h1 className="text-xl md:text-1xl xl:text-2xl">Top Tokens</h1>
+        <Button text={`View all (${count})`} href={`/${network}/tokens`} />
+      </div>
+      <TokensTable
+        data={results}
+        allowOrder={false}
+        isLoading={isLoading}
+        placeholderRows={5}
+        {...rest}
+      />
+    </>
   );
 };
 
