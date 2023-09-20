580ec379947aa6802fc68485e445547bbde9b197
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2023-09-20 15:42:34 +0200

    ci: sentry test

--- src/app/v2/tokens/page.jsx
+++ src/app/v2/tokens/page.jsx
@@ -58,6 +58,8 @@ const TokensPage = () => {
           className="mb-5"
         />
       </div>
+      {/* eslint-disable no-undef */}
+      <button onClick={() => methodDoesNotExist()}>Break the world</button>
 
       <TokensTable
         data={results}
