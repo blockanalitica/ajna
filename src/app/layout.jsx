6d4f6710c27f9624e9d9152b29066fef22a24cd1
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2023-09-26 14:31:23 +0200

    fix: only show notice for v1

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,15 +1,20 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { useLocation } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Notice from "./Notice";
 import { Outlet } from "react-router-dom";
+import { smartLocationParts } from "@/utils/url";
 
 const RootLayout = () => {
+  const location = useLocation();
+  const { version } = smartLocationParts(location);
+
   return (
     <main>
       <div className="mx-auto max-w-80 px-4">
         <Navbar />
-        <Notice />
+        {version === "v1" ? <Notice /> : null}
         <Outlet />
         <Footer />
       </div>
