13a90cb61f09f03bc10d750467fd477d6c1bbb6a
Tomaž Šifrer <3967564+tsifrer@users.noreply.github.com>
2023-11-03 14:55:04 +0100

    feat: stats history (#155)

--- src/app/v2/page.jsx
+++ src/app/v2/page.jsx
@@ -2,7 +2,10 @@ import { useQueryParams } from "@/hooks";
 import TotalStats from "./TotalStats";
 import TopPools from "./TopPools";
 import TopTokens from "./TopTokens";
+import HistoryStatsGraph from "./HistoryStatsGraph";
+import HistoryVolumeGraph from "./HistoryVolumeGraph";
 import DisplaySwitch from "@/components/switch/DisplaySwitch";
+import CardBackground from "@/components/card/CardBackground";
 
 const Page = () => {
   const { queryParams, setQueryParams } = useQueryParams();
@@ -19,6 +22,15 @@ const Page = () => {
       </section>
 
       <TotalStats className="mb-10" daysAgo={daysAgo} />
+
+      <div className="flex flex-col-reverse md:flex-row md:gap-4">
+        <CardBackground className="md:w-1/2 mb-10">
+          <HistoryStatsGraph />
+        </CardBackground>
+        <CardBackground className="md:w-1/2 mb-10">
+          <HistoryVolumeGraph />
+        </CardBackground>
+      </div>
 
       <div className="mb-10">
         <TopPools daysAgo={daysAgo} />
