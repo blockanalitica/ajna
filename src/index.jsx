e9c57f92ba34b6b4f3bac0ee9cc42cfa9457a224
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2024-01-10 12:32:16 +0100

    fix: sentry error boundary

--- src/index.jsx
+++ src/index.jsx
@@ -3,37 +3,29 @@ import ReactDOM from "react-dom/client";
 import * as Sentry from "@sentry/react";
 import "@/index.css";
 import App from "@/App";
-import {
-  BrowserRouter,
-  useLocation,
-  useNavigationType,
-  createRoutesFromChildren,
-  matchRoutes,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
   environment: "ajna",
-  integrations: [
-    new Sentry.BrowserTracing({
-      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
-        React.useEffect,
-        useLocation,
-        useNavigationType,
-        createRoutesFromChildren,
-        matchRoutes,
-      ),
-    }),
-  ],
+  integrations: [],
   tracesSampleRate: 0,
-  ignoreErrors: [/^Request failed with status code 404$/],
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Sentry.ErrorBoundary
+      fallback={
+        <div className="text-center mt-10">
+          <h1 className="text-4xl">ERROR</h1>
+          <p className="mt-10">Oops, it seems that something went wrong.</p>
+        </div>
+      }
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Sentry.ErrorBoundary>
   </React.StrictMode>,
 );
