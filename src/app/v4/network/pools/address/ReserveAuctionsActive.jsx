8306a92d53d446e50ae9e1c1a66c9e73d85781f2
Tomaž Šifrer <3967564+tsifrer@users.noreply.github.com>
2024-05-31 14:19:06 +0200

    feat: show estimated auction price on active reserve auction (#215)
    * feat: show estimated auction price on active reserve auction
    
    * feat: show est. auction price on reserve auction

--- src/app/v4/network/pools/address/ReserveAuctionsActive.jsx
+++ src/app/v4/network/pools/address/ReserveAuctionsActive.jsx
@@ -63,6 +63,21 @@ const ReserveAuctionsActive = ({ poolAddress }) => {
       orderField: "claimable_reserves_remaining",
     },
     {
+      header: "Est. Auction Price",
+      cell: ({ row }) => (
+        <CurrencyValue
+          value={row.auction_price}
+          currencySymbol="AJNA"
+          currencyAddress={AJNA_TOKEN_ADDRESS}
+          network="ethereum"
+          dashIfZero
+        />
+      ),
+      headerAlign: "end",
+      cellAlign: "end",
+      orderField: "last_take_price",
+    },
+    {
       header: "Last Take Price",
       cell: ({ row }) => (
         <CurrencyValue
