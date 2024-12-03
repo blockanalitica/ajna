b46b58971d991777a91ead5a9732aaadabfec22d
Aljosa Urbas <aljosaurbas@gmail.com>
2024-12-03 11:29:38 +0100

    feat: replaced medium with substack and twitter with x

--- src/app/Footer.jsx
+++ src/app/Footer.jsx
@@ -1,8 +1,8 @@
-import { faMedium, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ExternalLink from "@/components/externalLink/ExternalLink";
 import baLogo from "@/assets/images/logos/BlockAnalitica-Logo-white.svg";
-import sub from "@/assets/images/logos/test";
+import substack from "@/assets/images/logos/substack.svg";
 
 const Footer = () => (
   <footer className="mt-20 mb-10">
@@ -29,10 +29,13 @@ const Footer = () => (
           href="https://twitter.com/BlockAnalitica"
           className="w-10 h-10 bg-gray-21 rounded-full hover:bg-gray-18 flex items-center justify-center"
         >
-          <FontAwesomeIcon icon={faTwitter} />
+          <FontAwesomeIcon icon={faXTwitter} />
         </ExternalLink>
-        <ExternalLink href="https://blockanalitica.com">
-          <img alt="Block Analitica" src={sub} width="150" height="58" />
+        <ExternalLink
+          href="https://blockanalitica.substack.com/"
+          className="w-10 h-10 bg-gray-21 rounded-full hover:bg-gray-18 flex items-center justify-center"
+        >
+          <img alt="Substack" src={substack} width="15" height="15" />
         </ExternalLink>
       </div>
     </div>
